2a65670580931ce185936748ed536881821e2798
leminhnguyenai <leminhnguyenai@gmail.com>
2024-11-29 18:21:53 +0700

    Add more commands to help command list and remove flag for simplicity

--- student-mark-management/utils/commands_input.py
+++ student-mark-management/utils/commands_input.py
@@ -1,6 +1,11 @@
 def display_commands():
     print("-- Commands list --\n")
-    print("     -i students: Adding students to the class")
+    print("     i students: Add students to the class")
+    print("     i courses: Add courses")
+    print("     s courses: Select course to modify")
+    print("     ls courses: Show all courses")
+    print("     ls students: Show all students")
+    print("     ls marks: Show students' marks for given course")
 
 
 commandList = {"h": display_commands}
