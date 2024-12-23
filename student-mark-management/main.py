6db729aaa23003572d5207f003fe98acfc87ca48
leminhnguyenai <leminhnguyenai@gmail.com>
2024-12-23 11:43:40 +0700

    Remove lab1 files

--- student-mark-management/main.py
+++
@@ -1,5 +0,0 @@
-from utils import welcome
-import process_input
-
-welcome.print_welcome()
-process_input.listen_to_input()
--- student-mark-management/utils/welcome.py
+++
@@ -1,4 +0,0 @@
-def print_welcome():
-    print("STUDENTS' MARK MANAGEMENT")
-    print("v 1.0")
-    print("\nType h for help, 'quit' to quit")
